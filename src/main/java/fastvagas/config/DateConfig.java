71df380d2c5c04d10ea4ea680e430e6f0d716a31
Ricardo Campos <ricardo@ricardocampos.blog>
2021-07-14 08:11:47 -0300

    Cleanup at DateConfig class
    Unused imports removal.

--- src/main/java/fastvagas/config/DateConfig.java
+++ src/main/java/fastvagas/config/DateConfig.java
@@ -1,16 +1,13 @@
 package fastvagas.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ser.std.DateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
 
 @Configuration
 public class DateConfig {
