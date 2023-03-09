e7cdafb1d222fce9bae95d34df0d39c6787899fa
Ricardo Campos <ricardo.campos@encora.com>
2023-03-09 10:01:47 -0300

    tests: add tests

---
+++ src/test/resources/sql/PortalRepositoryTest.sql
@@ -0,0 +1,6 @@
+INSERT INTO fjobs.portal (name, search_url, city, state, enabled)
+VALUES
+  ('Portal One', 'https://portal-one.com/jobs', 'Victoria', 'BC', TRUE),
+  ('Portal Two', 'https://portal-two.com/jobs', 'Joinville', 'SC', TRUE),
+  ('Portal Three', 'https://portal-three.com/jobs', 'Porto', 'PT', TRUE),
+  ('Portal Four', 'https://portal-four.com/jobs', 'Curitiba', 'PR', FALSE);
